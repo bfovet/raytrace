bc537bd38d047cbce2e35af3d89c5be6c710f9d2
Benjamin Fovet <b.fovet@gmail.com>
2025-05-09 21:58:48 +0200

    refactor: move pixels generation to its own function

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@ use std::{fs, io};
 
 const MAX_VALUE: u8 = 255;
 
-/// Write a ppm file
-fn write_ppm_file(filename: &str, width: u32, height: u32) -> io::Result<()> {
-    let pixels = (0..height)
+fn generate_gradient(width: u32, height: u32) -> String {
+    (0..height)
         .cartesian_product(0..width)
         .progress_count(height as u64 * width as u64)
         .map(|(y, x)| {
-            let r = y as f64 / (width - 1) as f64;
-            let g = x as f64 / (height - 1) as f64;
+            let r = x as f64 / (width - 1) as f64;
+            let g = y as f64 / (height - 1) as f64;
             let b = 0.0;
 
             format!("{} {} {}", r * 255.0, g * 255.0, b * 255.0)
         })
-        .join("\n");
+        .join("\n")
+}
+
+/// Write a ppm file
+fn write_ppm_file(filename: &str, width: u32, height: u32) -> io::Result<()> {
+    let pixels = generate_gradient(width, height);
 
     fs::write(filename, format!(
         "P3
