138443f148b62ecee96cccce831c2a7629ba4f65
Benjamin Fovet <b.fovet@gmail.com>
2025-05-11 20:39:04 +0200

    feat: send rays into the scene

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,60 @@
 use indicatif::ProgressIterator;
 use itertools::Itertools;
+use lerp::Lerp;
 use nalgebra::Vector3;
 use std::{fs, io};
 
+const IMAGE_WIDTH: u32 = 400;
 const MAX_VALUE: u8 = 255;
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
+const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+const VIEWPORT_HEIGHT: f64 = 2.0;
+const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGHT * (IMAGE_WIDTH as f64 / IMAGE_HEIGHT as f64);
+const FOCAL_LENGTH: f64 = 1.0;
+const CAMERA_CENTER: Vector3<f64> = Vector3::new(0.0, 0.0, 0.0);
+
+// Calculate the vectors across the horizontal and down the vertical viewport edges
+const VIEWPORT_U: Vector3<f64> = Vector3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+const VIEWPORT_V: Vector3<f64> = Vector3::new(0.0, -VIEWPORT_HEIGHT, 0.0);
 
 fn generate_gradient(width: u32, height: u32) -> String {
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel
+    let pixel_delta_u = VIEWPORT_U / width as f64;
+    let pixel_delta_v = VIEWPORT_V / height as f64;
+
+    // Calculate the location of the upper left pixel
+    let viewport_upper_left =
+        CAMERA_CENTER - Vector3::new(0.0, 0.0, FOCAL_LENGTH) - VIEWPORT_U / 2.0 - VIEWPORT_V / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
     (0..height)
         .cartesian_product(0..width)
         .progress_count(height as u64 * width as u64)
         .map(|(y, x)| {
-            let r = x as f64 / (width - 1) as f64;
-            let g = y as f64 / (height - 1) as f64;
-            let b = 0.0;
+            let pixel_center =
+                pixel00_loc + (x as f64 * pixel_delta_u) + (y as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - CAMERA_CENTER;
+            let ray = Ray {
+                origin: CAMERA_CENTER,
+                direction: ray_direction,
+            };
 
-            format!(
-                "{} {} {}",
-                (r * 255.0) as u8,
-                (g * 255.0) as u8,
-                (b * 255.0) as u8
-            )
+            let pixel_color = ray.color() * MAX_VALUE as f64;
+
+            format!("{} {} {}", pixel_color.x as u8, pixel_color.y as u8, pixel_color.z as u8)
         })
         .join("\n")
 }
 
 /// Write a ppm file
-fn write_ppm_file(filename: &str, width: u32, height: u32) -> io::Result<()> {
-    let pixels = generate_gradient(width, height);
+fn write_ppm_file(filename: &str) -> io::Result<()> {
+    let pixels = generate_gradient(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     fs::write(
         filename,
         format!(
             "P3
-{width} {height}
+{IMAGE_WIDTH} {IMAGE_HEIGHT}
 {MAX_VALUE}
 {pixels}"
         ),
@@ -43,10 +65,8 @@ fn write_ppm_file(filename: &str, width: u32, height: u32) -> io::Result<()> {
 
 fn main() {
     let filename = "image.ppm";
-    let width = 256;
-    let height = 256;
 
-    if let Err(e) = write_ppm_file(filename, width, height) {
+    if let Err(e) = write_ppm_file(filename) {
         eprintln!("Error writing PPM file: {e}");
     } else {
         println!("PPM file '{filename}' created successfully.");
@@ -62,6 +82,12 @@ impl Ray {
     fn at(&self, t: f64) -> Vector3<f64> {
         self.origin + self.direction * t
     }
+
+    fn color(&self) -> Vector3<f64> {
+        let unit_direction = self.direction.normalize();
+        let a = 0.5 * (unit_direction.y + 1.0);
+        Vector3::new(1.0, 1.0, 1.0).lerp(Vector3::new(0.5, 0.7, 1.0), a)
+    }
 }
 
 #[cfg(test)]
@@ -70,10 +96,10 @@ mod tests {
 
     #[test]
     fn test_generate_gradient() {
-        let pixels = generate_gradient(3, 3);
+        let pixels = generate_gradient(4, 2);
         assert_eq!(
             pixels,
-            "0 0 0\n127 0 0\n255 0 0\n0 127 0\n127 127 0\n255 127 0\n0 255 0\n127 255 0\n255 255 0"
+            "172 205 255\n164 200 255\n164 200 255\n172 205 255\n209 227 255\n217 232 255\n217 232 255\n209 227 255"
         );
     }
 }
