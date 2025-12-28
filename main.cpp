761233ddf2d9c0e8ab674b08a072d4755322fca9
Benjamin Fovet <b.fovet@gmail.com>
2025-12-28 18:07:07 +0100

    feat: add a progress indicator

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ auto main() -> int
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
     for (int i = 0; i < image_width; i++) {
       const auto r = static_cast<double>(i) / (image_width - 1);
       const auto g = static_cast<double>(j) / (image_height - 1);
@@ -20,6 +21,8 @@ auto main() -> int
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
+
+  std::clog << "\rDone.                  \n";
 
   return 0;
 }
