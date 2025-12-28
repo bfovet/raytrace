94f8a9e6301da8e5e9fe9133d9656acb4ce45cb5
Benjamin Fovet <b.fovet@gmail.com>
2025-12-28 15:54:43 +0100

    feat: output a ppm image

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+auto main() -> int
+{
+  int image_width = 256;
+  int image_height = 256;
+
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+  for (int j = 0; j < image_height; j++) {
+    for (int i = 0; i < image_width; i++) {
+      const auto r = static_cast<double>(i) / (image_width - 1);
+      const auto g = static_cast<double>(j) / (image_height - 1);
+      const auto b = 0.0;
+
+      const int ir = static_cast<int>(255.999 * r);
+      const int ig = static_cast<int>(255.999 * g);
+      const int ib = static_cast<int>(255.999 * b);
+
+      std::cout << ir << " " << ig << " " << ib << "\n";
+    }
+  }
+
+  return 0;
+}
