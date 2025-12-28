3a2cfc79142577f303b72bb450f3124f5f71591c
Benjamin Fovet <b.fovet@gmail.com>
2025-12-28 18:11:30 +0100

    refactor: use vec3 and write_color

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include "color.hpp"
+
 #include <iostream>
 
 auto main() -> int
@@ -8,17 +10,12 @@ auto main() -> int
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
-    std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
+              << std::flush;
     for (int i = 0; i < image_width; i++) {
-      const auto r = static_cast<double>(i) / (image_width - 1);
-      const auto g = static_cast<double>(j) / (image_height - 1);
-      const auto b = 0.0;
-
-      const int ir = static_cast<int>(255.999 * r);
-      const int ig = static_cast<int>(255.999 * g);
-      const int ib = static_cast<int>(255.999 * b);
-
-      std::cout << ir << " " << ig << " " << ib << "\n";
+      auto pixel_color =
+        color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+      write_color(std::cout, pixel_color);
     }
   }
 
