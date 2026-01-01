27bd13e2ec3946ebe59f31ab770d19acc5991417
Benjamin Fovet <b.fovet@gmail.com>
2026-01-01 17:11:13 +0100

    refactor: make normals point outward

--- sphere.hpp
+++ sphere.hpp
@@ -39,7 +39,8 @@ public:
 
     rec.t = root;
     rec.p = r.at(rec.t);
-    rec.normal = (rec.p - center) / radius;
+    const vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
 
     return true;
   }
