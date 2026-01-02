07d44d1afbe2c7b249a041efdcedbb9d39c17976
Benjamin Fovet <b.fovet@gmail.com>
2026-01-02 13:57:40 +0100

    feat: model light scatter and reflectance

--- sphere.hpp
+++ sphere.hpp
@@ -10,9 +10,10 @@ public:
     : center(center),
       radius(std::fmax(0, radius))
   {
+    // TODO: Initialize the material pointer `mat`.
   }
 
-  bool hit(const ray& r, interval ray_t, hit_record& rec) const override
+  bool hit(const ray& r, const interval ray_t, hit_record& rec) const override
   {
     const vec3 oc = center - r.origin();
     const auto a = r.direction().length_squared();
@@ -39,6 +40,7 @@ public:
     rec.p = r.at(rec.t);
     const vec3 outward_normal = (rec.p - center) / radius;
     rec.set_face_normal(r, outward_normal);
+    rec.mat = mat;
 
     return true;
   }
@@ -46,4 +48,5 @@ public:
 private:
   point3 center;
   double radius;
+  shared_ptr<material> mat;
 };
