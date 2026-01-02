6b5b7698b1fcf104ef21bdd5640b447017c8e01c
Benjamin Fovet <b.fovet@gmail.com>
2026-01-02 14:04:44 +0100

    feat: add fuzzy reflection

--- material.hpp
+++ material.hpp
@@ -48,8 +48,9 @@ private:
 class metal : public material
 {
 public:
-  explicit metal(const color& albedo)
-    : albedo(albedo)
+  metal(const color& albedo, const double fuzz)
+    : albedo(albedo),
+      fuzz(fuzz < 1 ? fuzz : 1)
   {
   }
 
@@ -58,12 +59,14 @@ public:
     color& attenuation,
     ray& scattered) const override
   {
-    const vec3 reflected = reflect(r_in.direction(), rec.normal);
+    vec3 reflected = reflect(r_in.direction(), rec.normal);
+    reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
     scattered = ray(rec.p, reflected);
     attenuation = albedo;
-    return true;
+    return (dot(scattered.direction(), rec.normal) > 0);
   }
 
 private:
   color albedo;
+  double fuzz;
 };
